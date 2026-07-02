7c7af9af573945ff1690a9d3081ce79d1ae35f8f
Ujjval-crypto <Ujjvalchaturvedi14@gmail.com>
2026-07-02 20:38:07 +0530

    Add C program to print your name

---
+++ 0002 c program to print your name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  //you have to write your name
+  printf("Ujjval");
+
+  //you can write "return 0;" as well but I prefer getch(); 
+getch();
+}
